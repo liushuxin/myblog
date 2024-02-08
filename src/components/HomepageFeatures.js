b5e45c0ad0b61e4def3e31d720def9b924ac3381
刘述新 <liushuxin@bailongma-inc.com>
2024-02-08 15:50:06 +0800

    feat: 文案更新

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,19 +4,19 @@ import styles from "./HomepageFeatures.module.css";
 
 const FeatureList = [
   {
-    title: "追求卓越，不忘初心",
+    title: "悟已往之不谏，知来者之可追",
     Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
     description: <>努力做一枚优秀的前端工程师~</>,
   },
   {
-    title: "你好，5月",
+    title: "你好，2月",
     Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
-    description: <>愿爱我的人和我爱的人都能够平安幸福，</>,
+    description: <>过年啦~，愿新的一年，家人平安，在北京的日子能够顺顺利利</>,
   },
   {
-    title: "做该做的事情，并接受事与愿违",
+    title: "做自己该做的事情，并接受它的事与愿违",
     Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
-    description: <>凡是尽力就好</>,
+    description: <>凡事尽力就好~</>,
   },
 ];
 
